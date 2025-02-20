2fc06dbfe042d2b4d34193d834412037eaa6ec3f
Aleksandergreg <144420230+Aleksandergreg@users.noreply.github.com>
2025-02-20 12:27:40 +0100

    Update repositories.py

--- repositories.py
+++ repositories.py
@@ -13,10 +13,10 @@ GROUP_REPOS = [
         {
             "name": "DevOpsDynamite",
             "gitLinks": ["https://github.com/DevOpsDynamite/DevOpsDynamite_Python2to3"],
-            "backend": "",
-            "frontend": "",
+            "backend": "104.46.44.178:4568/api",
+            "frontend": "104.46.44.178:4568",
             "monitoring": "",
-            "stack": [],
+            "stack": ["Ruby", "Sinatra", "SQLite", "HTML/CSS/JS"],
             "documentation": [],
             "sla": "",
 
@@ -34,11 +34,11 @@ GROUP_REPOS = [
 {
             "name": "PipelinePionee.rs",
             "gitLinks": ["https://github.com/PipelinePionee-rs/Sauron.git"],
-            "backend": "",
-            "frontend": "",
+            "backend": "sauron.tolana.dev/api/v1",
+            "frontend": "sauron.tolana.dev",
             "monitoring": "",
             "stack": ["Rust","Axum","SQLite","HTML/CSS/JS"],
-            "documentation": [],
+            "documentation": ["sauron.tolana.dev/doc/api-doc/openapi.json"],
             "sla": "",
 },
   
