d41f927ff31d28138dc457fa42cfd5e5e4cca053
anderslatif <anderslatif@users.noreply.github.com>
2025-03-07 15:46:44 +0100

    Finished the Debug Docker-compose project

---
+++ 05._Debug_Docker-Compose/app/app/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+const app = express();
+
+import pkg from 'pg';
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: 'db',
+  database: 'postgres',
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(res.rows[0]);
+  }
+});
+
+app.get('/', (req, res) => {
+    res.send({ data: `Hello from the ${process.env.NODE_ENV} environment` });
+});
+
+app.listen(80, () => {
+    console.log('App running on port 80');
+});
