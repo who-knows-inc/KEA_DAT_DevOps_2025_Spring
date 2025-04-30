32e09e7b64a6cab292e8d54d8c5bafedbbbeb03f
madsfjeldberg <mafj0001@stud.kea.dk>
2025-04-30 23:20:18 +0200

    updated repositories.py

--- repositories.py
+++ repositories.py
@@ -36,7 +36,7 @@ GROUP_REPOS = [
             "gitLinks": ["https://github.com/PipelinePionee-rs/Sauron.git"],
             "backend": "https://sauron.tolana.dev",
             "frontend": "https://sauron.tolana.dev",
-            "monitoring": "",
+            "monitoring": "https://montitor.tolana.dev",
             "stack": ["Rust","Axum","SQLite","HTML/CSS/JS"],
             "documentation": ["sauron.tolana.dev/doc/api-doc/openapi.json"],
             "sla": "",
